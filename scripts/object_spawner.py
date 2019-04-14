c4dad17dba3fa8dac6ca7aeb1a72f55fddd0d16f
Roberto Zegers <rzegers@uc.cl>
2019-04-14 02:47:10 +0000

    Add rospkg query to find out package path Add exception to handle errors on communication with service

--- scripts/object_spawner.py
+++ scripts/object_spawner.py
@@ -1,12 +1,12 @@
 #! /usr/bin/env python
 
 import rospy
+import rospkg
 from gazebo_msgs.srv import SpawnModel
 from geometry_msgs.msg import Pose
 
 
 rospy.init_node('object_spawner', log_level=rospy.INFO)
-rospy.wait_for_service('gazebo/spawn_urdf_model')
 
 initial_pose = Pose()
 initial_pose.position.x = 0
@@ -14,13 +14,19 @@ initial_pose.position.y = 0
 initial_pose.position.z = 0.5
 
 # Spawn SDF model
+model_path = rospkg.RosPack().get_path('object_spawner')+'/models/'
+model_name = 'coke_can'
 model_xml = ''
 
-with open('../models/coke_can/model.sdf', 'r') as xml_file:
+with open(model_path + model_name + '/model.sdf', 'r') as xml_file:
     model_xml = xml_file.read().replace('\n', '')
 
-# create a handle for calling the service
-spawn_model_prox = rospy.ServiceProxy('gazebo/spawn_sdf_model', SpawnModel)
-# then use this handle just like a normal function and call it
-spawn_model_prox('auto_spawned_model_name',
-                 model_xml, '', initial_pose, 'world')
+try:
+    rospy.wait_for_service('gazebo/spawn_urdf_model',5.0)
+    # create a handle for calling the service
+    spawn_model_prox = rospy.ServiceProxy('gazebo/spawn_sdf_model', SpawnModel)
+    # then use this handle just like a normal function and call it
+    spawn_model_prox('auto_spawned_model_name',model_xml, '', initial_pose, 'world')
+
+except (rospy.ServiceException, rospy.ROSException), e:
+    rospy.logerr("Service call failed: %s" % (e,))
